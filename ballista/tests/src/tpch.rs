ae8e70ffb0f110f16f406db6d84897616988c3d5
Andy Grove <agrove@apache.org>
2024-09-15 09:44:09 -0600

    save

--- ballista/tests/src/tpch.rs
+++ ballista/tests/src/tpch.rs
@@ -15,28 +15,131 @@
 // specific language governing permissions and limitations
 // under the License.
 
+use std::fs;
+use ballista_scheduler::planner::DistributedPlanner;
 use datafusion::common::Result;
 use datafusion::physical_plan::displayable;
 use datafusion::prelude::{ParquetReadOptions, SessionContext};
 use std::fs::File;
 use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::path::Path;
-use ballista_scheduler::planner::DistributedPlanner;
+use tempfile::NamedTempFile;
 
 #[tokio::test]
-async fn test_tpch_plans() -> Result<()> {
-    for i in 1..=22 {
-        let name = format!("q{i}");
-        let _ = test_expected_tpch_plan(name.as_str()).await?;
-    }
-    Ok(())
+async fn test_tpch_q1() -> Result<()> {
+    test_expected_tpch_plan("q1").await
 }
 
+#[tokio::test]
+async fn test_tpch_q2() -> Result<()> {
+    test_expected_tpch_plan("q2").await
+}
+
+#[tokio::test]
+async fn test_tpch_q3() -> Result<()> {
+    test_expected_tpch_plan("q3").await
+}
+
+#[tokio::test]
+async fn test_tpch_q4() -> Result<()> {
+    test_expected_tpch_plan("q4").await
+}
+
+#[tokio::test]
+async fn test_tpch_q5() -> Result<()> {
+    test_expected_tpch_plan("q5").await
+}
+
+#[tokio::test]
+async fn test_tpch_q6() -> Result<()> {
+    test_expected_tpch_plan("q6").await
+}
+
+#[tokio::test]
+async fn test_tpch_q7() -> Result<()> {
+    test_expected_tpch_plan("q7").await
+}
+
+#[tokio::test]
+async fn test_tpch_q8() -> Result<()> {
+    test_expected_tpch_plan("q8").await
+}
+
+#[tokio::test]
+async fn test_tpch_q9() -> Result<()> {
+    test_expected_tpch_plan("q9").await
+}
+
+#[tokio::test]
+async fn test_tpch_q10() -> Result<()> {
+    test_expected_tpch_plan("q10").await
+}
+
+#[tokio::test]
+async fn test_tpch_q11() -> Result<()> {
+    test_expected_tpch_plan("q11").await
+}
+
+#[tokio::test]
+async fn test_tpch_q12() -> Result<()> {
+    test_expected_tpch_plan("q12").await
+}
+
+#[tokio::test]
+async fn test_tpch_q13() -> Result<()> {
+    test_expected_tpch_plan("q13").await
+}
+
+#[tokio::test]
+async fn test_tpch_q14() -> Result<()> {
+    test_expected_tpch_plan("q14").await
+}
+
+#[tokio::test]
+async fn test_tpch_q15() -> Result<()> {
+    test_expected_tpch_plan("q15").await
+}
+
+#[tokio::test]
+async fn test_tpch_q16() -> Result<()> {
+    test_expected_tpch_plan("q16").await
+}
+
+#[tokio::test]
+async fn test_tpch_q17() -> Result<()> {
+    test_expected_tpch_plan("q17").await
+}
+
+#[tokio::test]
+async fn test_tpch_q18() -> Result<()> {
+    test_expected_tpch_plan("q18").await
+}
+
+#[tokio::test]
+async fn test_tpch_q19() -> Result<()> {
+    test_expected_tpch_plan("q19").await
+}
+
+#[tokio::test]
+async fn test_tpch_q20() -> Result<()> {
+    test_expected_tpch_plan("q20").await
+}
+
+#[tokio::test]
+async fn test_tpch_q21() -> Result<()> {
+    test_expected_tpch_plan("q21").await
+}
+
+#[tokio::test]
+async fn test_tpch_q22() -> Result<()> {
+    test_expected_tpch_plan("q22").await
+}
 
 async fn test_expected_tpch_plan(name: &str) -> Result<()> {
     let ctx = SessionContext::new();
     for table in [
-        "customer", "nation", "part", "region", "lineitem", "orders", "partsupp", "supplier",
+        "customer", "nation", "part", "region", "lineitem", "orders", "partsupp",
+        "supplier",
     ] {
         let path = format!("/mnt/bigdata/tpch/sf100/{}.parquet", table);
         ctx.register_parquet(table, &path, ParquetReadOptions::default())
@@ -65,11 +168,25 @@ async fn test_expected_tpch_plan(name: &str) -> Result<()> {
     }
 
     let expected_plan_path = format!("tpch/expected-plans/{}.txt", name);
-    println!("Writing: {}", expected_plan_path);
+    if fs::exists(&expected_plan_path)? {
+        let temp_file = NamedTempFile::new()?;
+        write_plans(ctx, &mut sqls, temp_file.path()).await?;
+        let expected = fs::read_to_string(Path::new(&expected_plan_path))?;
+        let actual = fs::read_to_string(temp_file)?;
+        assert_eq!(expected, actual);
+    } else {
+        println!("Writing: {}", expected_plan_path);
+        write_plans(ctx, &mut sqls, Path::new(&expected_plan_path)).await?;
+    }
+
+    Ok(())
+}
+
+async fn write_plans(ctx: SessionContext, sqls: &mut Vec<String>, expected_plan_path: &Path) -> Result<()> {
     let file = File::create(expected_plan_path)?;
     let mut w = BufWriter::new(file);
 
-    for sql in &sqls {
+    for sql in sqls {
         write!(w, "{}\n\n", sql)?;
         let df = ctx.sql(&sql).await?;
         let physical_plan = ctx.state().create_physical_plan(df.logical_plan()).await?;
@@ -80,6 +197,5 @@ async fn test_expected_tpch_plan(name: &str) -> Result<()> {
             write!(w, "Query Stage {}:\n{}\n", idx, displayable).unwrap();
         });
     }
-
     Ok(())
 }
